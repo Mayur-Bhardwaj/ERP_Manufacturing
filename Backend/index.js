ba4fb45e4b5fc865eaaa69467f40bdb636798e5e
softo-mayur <mayurbhardwaj99@gmail.com>
2026-04-01 15:07:11 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,28 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 const app = express();
 const port = 5000;
 
+// middleware (important for future APIs)
+app.use(express.json());
+
+// home route
 app.get('/', (req, res) =>{
   res.send("Hurry !! Server is runnig.");
+});
+
+// test api
+app.get('/test', async (req, res) =>{
+ try{
+  const users = await prisma.user.findMany();
+  res.json(users);
+ } catch(error){
+  console.log(error);
+  res.status(500).json({error: "Something went wrong !!"});
+ }
 });
 
 app.listen(port, ()=>{
