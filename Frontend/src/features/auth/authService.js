043f9a7c7bb4f5f6a566344948f1b31eccd31486
softo-mayur <mayurbhardwaj99@gmail.com>
2026-04-20 17:13:51 +0530

    Start Frontend

--- Frontend/src/features/auth/authService.js
+++
@@ -1,7 +0,0 @@
-// import axiosInstance from "../../services/axiosInstance";
-import axiosInstance from "../../services/axiosInstance";
-
-export const loginUser = async (loginData) => {
-  const response = await axiosInstance.post("/auth/login", loginData);
-  return response.data;
-};
